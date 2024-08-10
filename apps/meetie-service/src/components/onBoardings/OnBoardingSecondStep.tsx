7386f7b79980a664e33b17a6ac9a92d2fec5a368
ejinn1 <ejinn1@naver.com>
2024-08-10 21:08:29 +0900

    feat: onBoarding 두번째 step 클릭 추가 (#4)

--- apps/meetie-service/src/components/onBoardings/OnBoardingSecondStep.tsx
+++ apps/meetie-service/src/components/onBoardings/OnBoardingSecondStep.tsx
@@ -1,35 +1,34 @@
+import { useState } from "react";
+
 export default function OnBoardingSecondStep() {
+  const PURPOSES = ["자기 개발", "툴 능력 향상", "해당 분야의 네트워킹 확장", "취미"];
+
+  const [clickedPurpose, setClickedPurpose] = useState<string[]>([]);
+
+  const handleClickPurpose = (newPurpose: string) => {
+    if (clickedPurpose.includes(newPurpose)) {
+      setClickedPurpose((prevs) => prevs.filter((prev) => prev !== newPurpose));
+    } else {
+      setClickedPurpose((prev) => [...prev, newPurpose]);
+    }
+  };
+
   return (
     <article className="flex flex-col h-full w-full items-center p-[16px]">
       <div className="w-full pt-[60px] flex flex-col gap-[12px] justify-center items-start text-regular-16">
-        <label className="flex items-center gap-[8px] border border-gray-100 p-[10px] rounded-lg peer-checked:bg-primary-400 peer-checked:text-primary-500 peer-checked:border-primary-500">
-          <input
-            type="checkbox"
-            className="peer w-[20px] h-[20px] rounded-full bg-gray-100 appearance-none checked:bg-primary-500"
-          />
-          자기 개발
-        </label>
-        <label className="flex items-center gap-[8px] border border-gray-100 p-[10px] rounded-lg checked:text-primary-500">
-          <input
-            type="checkbox"
-            className="w-[20px] h-[20px] rounded-full bg-gray-100 appearance-none checked:text-primary-500 checked:bg-primary-400"
-          />
-          툴 능력 향상
-        </label>
-        <label className="flex items-center gap-[8px] border border-gray-100 p-[10px] rounded-lg">
-          <input
-            type="checkbox"
-            className="w-[20px] h-[20px] rounded-full bg-gray-100 appearance-none checked:bg-primary-500"
-          />
-          해당 분야의 네트워킹 확장
-        </label>
-        <label className="flex items-center gap-[8px] border border-gray-100 p-[10px] rounded-lg">
-          <input
-            type="checkbox"
-            className="w-[20px] h-[20px] rounded-full bg-gray-100 appearance-none checked:bg-primary-500"
-          />
-          취미
-        </label>
+        {PURPOSES.map((purpose) => (
+          // 공통 컴포넌트로 변경
+          <button
+            value={purpose}
+            onClick={() => handleClickPurpose(purpose)}
+            className={`flex items-center gap-[8px] border p-[10px] rounded-lg ${clickedPurpose.includes(purpose) ? "border-primary-500 bg-primary-200 text-primary-500" : "border-gray-100"}`}
+          >
+            <div
+              className={`w-[20px] h-[20px] rounded-full ${clickedPurpose.includes(purpose) ? "bg-primary-500" : "bg-gray-100"}`}
+            ></div>
+            {purpose}
+          </button>
+        ))}
         <button className="flex items-center border border-gray-100 p-[10px] text-gray-200 bg-gray-50 rounded-lg">
           + 직접 입력하기
         </button>
