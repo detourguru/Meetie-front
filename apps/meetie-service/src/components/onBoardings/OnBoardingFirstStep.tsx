cf86ddf78ce200e34700485937d01e9130b1aaee
ejinn1 <ejinn1@naver.com>
2024-08-10 16:59:30 +0900

    feat: onBoarding 첫번째 step 클릭 추가 (#4)

--- apps/meetie-service/src/components/onBoardings/OnBoardingFirstStep.tsx
+++ apps/meetie-service/src/components/onBoardings/OnBoardingFirstStep.tsx
@@ -1,19 +1,54 @@
+import { useState } from "react";
+
+type Jobs = "designer" | "developer" | "planner";
+
 export default function OnBoardingFirstStep() {
+  const [clickedJob, setClickedJob] = useState<Jobs | null>(null);
+
+  const handleClickJob = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const value = e.currentTarget.value as Jobs;
+    setClickedJob(value);
+  };
+
   return (
     <article className="flex flex-col h-full w-full items-center p-[16px]">
-      <div className="flex justify-center items-center w-full gap-[8px] pt-[60px] text-regular-16">
-        <div className="w-[109px] h-[120px] rounded-lg bg-gray-50 flex flex-col items-center justify-center gap-[20px]">
+      <div className="flex justify-center items-center w-full gap-[8px] pt-[60px]">
+        <button
+          value="designer"
+          onClick={(e) => handleClickJob(e)}
+          className={`w-[109px] h-[120px] rounded-lg flex flex-col items-center justify-center gap-[20px] ${
+            clickedJob === "designer"
+              ? "bg-primary-200 text-primary-500 border border-primary-500 text-medium-16"
+              : "bg-gray-50 text-regular-16"
+          }`}
+        >
           <div>아이콘</div>
-          <div>텍스트</div>
-        </div>
-        <div className="w-[109px] h-[120px] rounded-lg bg-gray-50 flex flex-col items-center justify-center gap-[20px]">
+          <div>디자이너</div>
+        </button>
+        <button
+          value="developer"
+          onClick={(e) => handleClickJob(e)}
+          className={`w-[109px] h-[120px] rounded-lg flex flex-col items-center justify-center gap-[20px] ${
+            clickedJob === "developer"
+              ? "bg-primary-200 text-primary-500 border border-primary-500 text-medium-16"
+              : "bg-gray-50 text-regular-16"
+          }`}
+        >
           <div>아이콘</div>
-          <div>텍스트</div>
-        </div>
-        <div className="w-[109px] h-[120px] rounded-lg bg-gray-50 flex flex-col items-center justify-center gap-[20px]">
+          <div>개발자</div>
+        </button>
+        <button
+          value="planner"
+          onClick={(e) => handleClickJob(e)}
+          className={`w-[109px] h-[120px] rounded-lg flex flex-col items-center justify-center gap-[20px] ${
+            clickedJob === "planner"
+              ? "bg-primary-200 text-primary-500 border border-primary-500 text-medium-16"
+              : "bg-gray-50 text-regular-16"
+          }`}
+        >
           <div>아이콘</div>
-          <div>텍스트</div>
-        </div>
+          <div>기획자</div>
+        </button>
       </div>
     </article>
   );
