957a2c18cf7dc86bb1fea0e143515cc634fd4151
ejinn1 <ejinn1@naver.com>
2024-08-10 21:26:36 +0900

    feat: onBoarding third page click handler add (#4)

--- apps/meetie-service/src/components/onBoardings/OnBoardingThirdStep.tsx
+++ apps/meetie-service/src/components/onBoardings/OnBoardingThirdStep.tsx
@@ -1,30 +1,47 @@
+import { useState } from "react";
+
 export default function OnBoardingThirdStep() {
+  const STYLES = [
+    "주도적인",
+    "열정적인",
+    "손이 빠른",
+    "시간을 지키는",
+    "꼼꼼한",
+    "모험적인",
+    "신중한",
+    "커뮤니케이션에 능숙한",
+    "논리적인",
+    "파워 J",
+    " 분석적인",
+    "동기부여가 필요한",
+    "완벽주의",
+  ];
+
+  const [clickedStyle, setClickedStyle] = useState<string[]>([]);
+
+  const handleClickStyle = (newStyle: string) => {
+    if (clickedStyle.includes(newStyle)) {
+      setClickedStyle((prevs) => prevs.filter((prev) => prev !== newStyle));
+    } else {
+      setClickedStyle((prev) => [...prev, newStyle]);
+    }
+  };
+
   return (
     <article className="flex flex-col h-full w-full items-center p-[16px]">
       <div className="flex flex-wrap gap-[8px]">
-        <button
-          type="button"
-          className="border border-gray-100 rounded-lg text-regular-16 py-[10.5px] px-[12px]"
-        >
-          주도적인
-        </button>
-        <button
-          type="button"
-          className="border border-gray-100 rounded-lg text-regular-16 py-[10.5px] px-[12px]"
-        >
-          열정적인
-        </button>
-        <button
-          type="button"
-          className="border border-gray-100 rounded-lg text-regular-16 py-[10.5px] px-[12px]"
-        >
-          손이 빠른
-        </button>
-        <button
-          type="button"
-          className="border border-gray-100 rounded-lg text-regular-16 py-[10.5px] px-[12px]"
-        >
-          시간을 지키는
+        {STYLES.map((style) => (
+          // 공통 컴포넌트로 변경
+          <button
+            value={style}
+            onClick={() => handleClickStyle(style)}
+            className={`flex items-center gap-[8px] border p-[10px] rounded-lg ${clickedStyle.includes(style) ? "border-primary-500 bg-primary-200 text-primary-500" : "border-gray-100"}`}
+          >
+            {style}
+          </button>
+        ))}
+        <button className="flex items-center border border-gray-100 p-[10px] text-gray-200 bg-gray-50 rounded-lg">
+          + 직접 입력하기
         </button>
       </div>
     </article>
