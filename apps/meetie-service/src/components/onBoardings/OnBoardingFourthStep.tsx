16d56bdcdfde5c5fbf18c81707791e3cbcb11873
ejinn1 <ejinn1@naver.com>
2024-08-10 21:28:50 +0900

    feat: onBoarding fouth page click handler add (#4)

--- apps/meetie-service/src/components/onBoardings/OnBoardingFourthStep.tsx
+++ apps/meetie-service/src/components/onBoardings/OnBoardingFourthStep.tsx
@@ -1,35 +1,32 @@
+import { useState } from "react";
+
 export default function OnBoardingFourthStep() {
+  const PERIODS = ["1개월 이내", "1개월 ~ 3개월", "3개월 ~ 6개월", "6개월 이상"];
+
+  const [clickedPeriod, setClickedPeriod] = useState<string[]>([]);
+
+  const handleClickPeriod = (newPeriod: string) => {
+    if (clickedPeriod.includes(newPeriod)) {
+      setClickedPeriod((prevs) => prevs.filter((prev) => prev !== newPeriod));
+    } else {
+      setClickedPeriod((prev) => [...prev, newPeriod]);
+    }
+  };
   return (
     <article className="flex flex-col h-full w-full items-center p-[16px]">
       <div className="w-full pt-[60px] flex flex-col gap-[12px] justify-center items-start text-regular-16">
-        <label className="flex items-center gap-[8px] border border-gray-100 p-[10px] rounded-lg peer-checked:bg-primary-400 peer-checked:text-primary-500 peer-checked:border-primary-500">
-          <input
-            type="checkbox"
-            className="peer w-[20px] h-[20px] rounded-full bg-gray-100 appearance-none checked:bg-primary-500"
-          />
-          1개월 이내
-        </label>
-        <label className="flex items-center gap-[8px] border border-gray-100 p-[10px] rounded-lg checked:text-primary-500">
-          <input
-            type="checkbox"
-            className="w-[20px] h-[20px] rounded-full bg-gray-100 appearance-none checked:text-primary-500 checked:bg-primary-400"
-          />
-          1개월 ~ 3개월
-        </label>
-        <label className="flex items-center gap-[8px] border border-gray-100 p-[10px] rounded-lg checked:text-primary-500">
-          <input
-            type="checkbox"
-            className="w-[20px] h-[20px] rounded-full bg-gray-100 appearance-none checked:text-primary-500 checked:bg-primary-400"
-          />
-          3개월 ~ 6개월
-        </label>
-        <label className="flex items-center gap-[8px] border border-gray-100 p-[10px] rounded-lg checked:text-primary-500">
-          <input
-            type="checkbox"
-            className="w-[20px] h-[20px] rounded-full bg-gray-100 appearance-none checked:text-primary-500 checked:bg-primary-400"
-          />
-          6개월 이상
-        </label>
+        {PERIODS.map((period) => (
+          <button
+            value={period}
+            onClick={() => handleClickPeriod(period)}
+            className={`flex items-center gap-[8px] border p-[10px] rounded-lg ${clickedPeriod.includes(period) ? "border-primary-500 bg-primary-200 text-primary-500" : "border-gray-100"}`}
+          >
+            <div
+              className={`w-[20px] h-[20px] rounded-full ${clickedPeriod.includes(period) ? "bg-primary-500" : "bg-gray-100"}`}
+            ></div>
+            {period}
+          </button>
+        ))}
       </div>
     </article>
   );
