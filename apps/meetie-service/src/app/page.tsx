0787571f6a4f370e71d0a26b9d34f6edbc1ba841
sikkzz <sling0623@gmail.com>
2024-08-12 16:33:17 +0900

    refactor: unused code delete (#12)

--- apps/meetie-service/src/app/page.tsx
+++ apps/meetie-service/src/app/page.tsx
@@ -1,13 +1,5 @@
-import Button from "@/components/common/Button/Button";
 import Gnb from "@/components/common/Gnb/Gnb";
 
 export default function Home() {
-  return (
-    <>
-      <Button variant="outlinePrimary" size="lg">
-        button
-      </Button>
-      <Gnb />
-    </>
-  );
+  return <Gnb />;
 }
