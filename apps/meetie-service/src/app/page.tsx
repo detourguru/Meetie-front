652ff34971795699087397a24d612adbc6e90a34
sikkzz <sling0623@gmail.com>
2024-08-12 17:35:18 +0900

    feat: create tag button component (#14)

--- apps/meetie-service/src/app/page.tsx
+++ apps/meetie-service/src/app/page.tsx
@@ -1,5 +1,15 @@
-import Gnb from "@/components/common/Gnb/Gnb";
+import TagButton from "@/components/common/TagButton/TagButton";
 
 export default function Home() {
-  return <Gnb />;
+  return (
+    <div className="mt-10 rounded-xl">
+      <TagButton hasIcon>커뮤니케이션에 능숙한</TagButton>
+      <TagButton variant="select" hasIcon>
+        커뮤니케이션에 능숙한
+      </TagButton>
+      <TagButton variant="select">커뮤니케이션에 능숙한</TagButton>
+      <TagButton>커뮤니케이션에 능숙한</TagButton>
+      <TagButton variant="add" />
+    </div>
+  );
 }
