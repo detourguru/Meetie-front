655939a478fa12fcb2ac96c4eacff4f8d89551de
sikkzz <sling0623@gmail.com>
2024-08-12 17:44:19 +0900

    refactor: unused code delete (#14)

--- apps/meetie-service/src/app/page.tsx
+++ apps/meetie-service/src/app/page.tsx
@@ -1,15 +1,3 @@
-import TagButton from "@/components/common/TagButton/TagButton";
-
 export default function Home() {
-  return (
-    <div className="mt-10 rounded-xl">
-      <TagButton hasIcon>커뮤니케이션에 능숙한</TagButton>
-      <TagButton variant="select" hasIcon>
-        커뮤니케이션에 능숙한
-      </TagButton>
-      <TagButton variant="select">커뮤니케이션에 능숙한</TagButton>
-      <TagButton>커뮤니케이션에 능숙한</TagButton>
-      <TagButton variant="add" />
-    </div>
-  );
+  return <div>page</div>;
 }
