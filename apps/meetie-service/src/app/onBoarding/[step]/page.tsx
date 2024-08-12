70b5f570096ff17c5c9bb90961e98368d9a1c78d
ejinn1 <ejinn1@naver.com>
2024-08-12 17:41:35 +0900

    fix: onBoarding routing 주소 수정, main 태그 변경, 404 리다이렉트 추가 (#4)

--- apps/meetie-service/src/app/onBoarding/[step]/page.tsx
+++ apps/meetie-service/src/app/onBoarding/[step]/page.tsx
@@ -4,70 +4,69 @@ import OnBoardingFirstStep from "@/components/onBoardings/OnBoardingFirstStep";
 import OnBoardingFourthStep from "@/components/onBoardings/OnBoardingFourthStep";
 import OnBoardingSecondStep from "@/components/onBoardings/OnBoardingSecondStep";
 import OnBoardingThirdStep from "@/components/onBoardings/OnBoardingThirdStep";
-import { usePathname, useRouter } from "next/navigation";
+import { notFound, useParams, useRouter } from "next/navigation";
+
+const QUESTION = [
+  {
+    TITLE: { FIRST: "김서희님이 관심있는", SECOND: "직무는 무엇인가요?" },
+    SUBTEXT: "선택한 직무를 바탕으로 스터디를 추천해줄게요!",
+  },
+  {
+    TITLE: { FIRST: "김서희님의", SECOND: "스터디 목적은 무엇인가요?" },
+    SUBTEXT: "중복선택도 가능해요",
+  },
+  {
+    TITLE: { FIRST: "김서희님은", SECOND: "어떤 스타일이신가요?" },
+    SUBTEXT: "미티님과 비슷하다고 생각되는 키워드를 모두 선택해주세요",
+  },
+  {
+    TITLE: { FIRST: "김서희님의", SECOND: "예상 스터디 기간은 얼마인가요?" },
+    SUBTEXT: "나와 비슷한 유저들과 스터디할 수 있도록 도와드려요!",
+  },
+];
+
+const STEPS = ["job", "purpose", "style", "period"];
 
 export default function OnBoardingPage() {
-  const pathName = usePathname();
   const navigate = useRouter();
-  const step = Number(pathName.split("/").pop());
-
-  const QUESTION = [
-    {
-      TITLE: { FIRST: "김서희님이 관심있는", SECOND: "직무는 무엇인가요?" },
-      SUBTEXT: "선택한 직무를 바탕으로 스터디를 추천해줄게요!",
-    },
-    {
-      TITLE: { FIRST: "김서희님의", SECOND: "스터디 목적은 무엇인가요?" },
-      SUBTEXT: "중복선택도 가능해요",
-    },
-    {
-      TITLE: { FIRST: "김서희님은", SECOND: "어떤 스타일이신가요?" },
-      SUBTEXT: "미티님과 비슷하다고 생각되는 키워드를 모두 선택해주세요",
-    },
-    {
-      TITLE: { FIRST: "김서희님의", SECOND: "예상 스터디 기간은 얼마인가요?" },
-      SUBTEXT: "나와 비슷한 유저들과 스터디할 수 있도록 도와드려요!",
-    },
-  ];
+  const params = useParams();
+  const step = params.step as string;
+  const currentStepIndex = STEPS.indexOf(step);
 
   const handlePrevStep = () => {
-    if (step === 1) {
-      return;
-    }
-
-    navigate.push(`/onBoarding/${step - 1}`);
+    currentStepIndex === 0 || navigate.push(`/onBoarding/${STEPS[currentStepIndex - 1]}`);
   };
 
   const handleNextStep = () => {
-    if (step === 4) {
-      return;
-    }
-
-    navigate.push(`/onBoarding/${step + 1}`);
+    currentStepIndex === 3 || navigate.push(`/onBoarding/${STEPS[currentStepIndex + 1]}`);
   };
 
+  if (!STEPS.includes(step)) {
+    return notFound();
+  }
+
   return (
-    <div className="flex flex-col h-full">
+    <main className="flex flex-col h-full">
       <div className="p-[16px] flex justify-between items-center text-gray-200 text-medium-14">
         <p>
-          <span className="text-black">{step}</span>/4
+          <span className="text-black">{currentStepIndex + 1}</span>/4
         </p>
         <button className="uppercase ">skip</button>
       </div>
 
-      <main className="flex flex-col h-max">
+      <article className="flex flex-col h-max">
         <div className="w-full pt-[54px] px-[16px]">
           <h1 className="text-semibold-24 text-start mb-[20px]">
-            <span className="block">{step && QUESTION[step - 1].TITLE.FIRST}</span>
-            <span className="block">{step && QUESTION[step - 1].TITLE.SECOND}</span>
+            <span className="block">{QUESTION[currentStepIndex].TITLE.FIRST}</span>
+            <span className="block">{QUESTION[currentStepIndex].TITLE.SECOND}</span>
           </h1>
-          <p className="text-gray-200 text-regular-14">{step && QUESTION[step - 1].SUBTEXT}</p>
+          <p className="text-gray-200 text-regular-14">{QUESTION[currentStepIndex].SUBTEXT}</p>
         </div>
-        {step === 1 && <OnBoardingFirstStep />}
-        {step === 2 && <OnBoardingSecondStep />}
-        {step === 3 && <OnBoardingThirdStep />}
-        {step === 4 && <OnBoardingFourthStep />}
-      </main>
+        {currentStepIndex === 0 && <OnBoardingFirstStep />}
+        {currentStepIndex === 1 && <OnBoardingSecondStep />}
+        {currentStepIndex === 2 && <OnBoardingThirdStep />}
+        {currentStepIndex === 3 && <OnBoardingFourthStep />}
+      </article>
 
       <div className="mt-auto px-[16px] pb-[42px]">
         <p className="text-center text-gray-200 text-medium-12 mb-[13px]">
@@ -84,10 +83,10 @@ export default function OnBoardingPage() {
             onClick={handleNextStep}
             className="text-bold-16 bg-primary-300 text-white w-[206px] h-[49px] rounded-lg"
           >
-            {step !== 4 ? "다음" : "이제 마지막에요"}
+            {currentStepIndex !== 3 ? "다음" : "이제 마지막에요"}
           </button>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
