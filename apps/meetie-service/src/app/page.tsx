44954d748d86bdd42199d5704a4ebc3208dbe30b
ejinn1 <ejinn1@naver.com>
2024-08-12 15:43:03 +0900

    fix: /home -> / 으로 변경, 변경 예정 주석 추가, 하단 before 추가

--- apps/meetie-service/src/app/page.tsx
+++ apps/meetie-service/src/app/page.tsx
@@ -1,55 +1,35 @@
-import Header from "@/components/common/Header/Header";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <Header>
-        <Header.Title>스터디룸</Header.Title>
-        <Header.RightButton icon="svg/ic-header-question.svg" />
-      </Header>
+    <div className="flex flex-col h-screen">
+      <Link href="/login">
+        <main className="flex flex-col h-full">
+          <article className="flex flex-col h-full items-center pt-[104px]">
+            <div className="flex flex-col items-center gap-[8px] mb-[60px]">
+              <p className="text-semibold-18">같은 목표로 공부중인 유저</p>
+              <p className="text-bold-24 text-primary-400">124,368 명</p>
+            </div>
 
-      {/* <Header>
-        <Header.Title>탐색하기</Header.Title>
-      </Header> */}
+            {/* todo 이미지 변경 예정 */}
+            <div className="mb-[70px] w-[212px] h-[240px] bg-primary-400">로켓 이미지</div>
 
-      {/* <Header>
-        <Header.LeftButton />
-        <Header.Title hasButton>스터디 만들기</Header.Title>
-        <Header.RightText nowStep={1} />
-      </Header> */}
+            {/* todo icon 변경 예정 */}
+            <div className="flex justify-center items-center gap-[23px]">
+              <button className="w-[46px] h-[46px] rounded-full bg-black text-white">N</button>
+              <button className="w-[46px] h-[46px] rounded-full bg-black text-white">C</button>
+              <button className="w-[46px] h-[46px] rounded-full bg-black text-white">G</button>
+              <button className="w-[46px] h-[46px] rounded-full bg-black text-white">...</button>
+            </div>
+          </article>
+        </main>
 
-      {/* <Header>
-        <Header.LeftButton />
-        <Header.Title hasButton>오픈 프로필</Header.Title>
-      </Header> */}
-
-      {/* <Header>
-        <Header.LeftButton />
-        <Header.RightButton icon="svg/ic-header-more.svg" />
-      </Header> */}
-
-      {/* <Header backgroundColor="bg-[#f1f1ff]">
-        <Header.Title>스터디룸</Header.Title>
-        <Header.RightButton icon="svg/ic-header-plus.svg" />
-      </Header> */}
-
-      {/* <Header>
-        <Header.LeftButton />
-        <Header.Title hasButton>과제 인증</Header.Title>
-        <Header.RightButton icon="svg/ic-header-calendar.svg" />
-      </Header> */}
-
-      {/* <Header>
-        <Header.LeftButton isCloseIcon />
-        <Header.Title hasButton>최근 항목</Header.Title>
-        <Header.RightTextButton isComplete />
-      </Header> */}
-
-      {/* <Header>
-        <Header.LeftButton />
-        <Header.Title hasButton>내 능력 현황</Header.Title>
-        <Header.RightButton icon="svg/ic-header-question.svg" />
-      </Header> */}
-    </>
+        <div className="flex justify-center items-center h-[16px] text-gray-200 text-medium-12 mt-auto mb-[120px]">
+          <div>회원가입하기</div>
+          <div className="before:content-['|'] before:mx-[12px]">아이디 찾기</div>
+          <div className="before:content-['|'] before:mx-[12px]">비밀번호찾기</div>
+        </div>
+      </Link>
+    </div>
   );
 }
