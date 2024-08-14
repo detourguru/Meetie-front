49ac4e5f7d11e4ca904843224b99006301415bcc
ejinn1 <ejinn1@naver.com>
2024-08-14 12:26:38 +0900

    refactor: click handler 한 줄로 수정, key 값 추가 (#4)

--- apps/meetie-service/src/components/onBoardings/OnBoardingFourthStep.tsx
+++ apps/meetie-service/src/components/onBoardings/OnBoardingFourthStep.tsx
@@ -6,24 +6,23 @@ export default function OnBoardingFourthStep() {
   const [clickedPeriod, setClickedPeriod] = useState<string>("");
 
   const handleClickPeriod = (newPeriod: string) => {
-    if (clickedPeriod !== newPeriod) {
-      setClickedPeriod(newPeriod);
-    } else {
-      setClickedPeriod("");
-    }
+    clickedPeriod !== newPeriod ? setClickedPeriod(newPeriod) : setClickedPeriod("");
   };
+
   return (
     <div className="flex flex-col h-full w-full items-center px-[16px]">
       <div className="w-full pt-[60px] flex flex-col gap-[12px] justify-center items-start text-regular-16">
-        {PERIODS.map((period) => (
+        {PERIODS.map((period, index) => (
+          // TODO: 공통 컴포넌트로 변경 예정
           <button
+            key={`period${index}`}
             value={period}
             onClick={() => handleClickPeriod(period)}
             className={`flex items-center gap-[8px] border p-[10px] rounded-lg ${clickedPeriod.includes(period) ? "border-primary-500 bg-primary-200 text-primary-500" : "border-gray-100"}`}
           >
             <div
               className={`w-[20px] h-[20px] rounded-full ${clickedPeriod.includes(period) ? "bg-primary-500" : "bg-gray-100"}`}
-            ></div>
+            />
             {period}
           </button>
         ))}
