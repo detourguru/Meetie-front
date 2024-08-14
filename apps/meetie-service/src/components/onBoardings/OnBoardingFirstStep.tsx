056ef7ed09cd50b5a1831180b5e35aef54b86c66
ejinn1 <ejinn1@naver.com>
2024-08-14 12:14:13 +0900

    refactor: button map으로 축약, click handler 한 줄로 축약, jobs 상수화 (#4)

--- apps/meetie-service/src/components/onBoardings/OnBoardingFirstStep.tsx
+++ apps/meetie-service/src/components/onBoardings/OnBoardingFirstStep.tsx
@@ -1,54 +1,34 @@
 import { useState } from "react";
 
-type Jobs = "designer" | "developer" | "planner";
+const JOBS = ["designer", "developer", "planner"];
+const JOBS_KR = ["디자이너", "개발자", "기획자"];
 
 export default function OnBoardingFirstStep() {
-  const [clickedJob, setClickedJob] = useState<Jobs | null>(null);
+  const [clickedJob, setClickedJob] = useState<string | null>(null);
 
   const handleClickJob = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    const value = e.currentTarget.value as Jobs;
-    setClickedJob(value);
+    setClickedJob(e.currentTarget.value);
   };
 
   return (
     <div className="flex flex-col h-full w-full items-center px-[16px]">
       <div className="flex justify-center items-center w-full gap-[8px] pt-[60px]">
-        <button
-          value="designer"
-          onClick={(e) => handleClickJob(e)}
-          className={`w-[109px] h-[120px] rounded-lg flex flex-col items-center justify-center gap-[20px] ${
-            clickedJob === "designer"
-              ? "bg-primary-200 text-primary-500 border border-primary-500 text-medium-16"
-              : "bg-gray-50 text-regular-16"
-          }`}
-        >
-          <div>아이콘</div>
-          <div>디자이너</div>
-        </button>
-        <button
-          value="developer"
-          onClick={(e) => handleClickJob(e)}
-          className={`w-[109px] h-[120px] rounded-lg flex flex-col items-center justify-center gap-[20px] ${
-            clickedJob === "developer"
-              ? "bg-primary-200 text-primary-500 border border-primary-500 text-medium-16"
-              : "bg-gray-50 text-regular-16"
-          }`}
-        >
-          <div>아이콘</div>
-          <div>개발자</div>
-        </button>
-        <button
-          value="planner"
-          onClick={(e) => handleClickJob(e)}
-          className={`w-[109px] h-[120px] rounded-lg flex flex-col items-center justify-center gap-[20px] ${
-            clickedJob === "planner"
-              ? "bg-primary-200 text-primary-500 border border-primary-500 text-medium-16"
-              : "bg-gray-50 text-regular-16"
-          }`}
-        >
-          <div>아이콘</div>
-          <div>기획자</div>
-        </button>
+        {JOBS.map((job, index) => (
+          <button
+            key={`jobs${index}`}
+            value={job}
+            onClick={(e) => handleClickJob(e)}
+            className={`w-[109px] h-[120px] rounded-lg flex flex-col items-center justify-center gap-[20px] ${
+              clickedJob === job
+                ? "bg-primary-200 text-primary-500 border border-primary-500 text-medium-16"
+                : "bg-gray-50 text-regular-16"
+            }`}
+          >
+            {/* TODO: icon 추가 예정 */}
+            <div>아이콘</div>
+            <p>{JOBS_KR[index]}</p>
+          </button>
+        ))}
       </div>
     </div>
   );
