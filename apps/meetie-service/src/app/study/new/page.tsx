b81fa6be7fcf57f65c304a3527b1b53a155252e5
sikkzz <sling0623@gmail.com>
2024-08-15 14:02:41 +0900

    feat: create progress bar component (#21)

--- apps/meetie-service/src/app/study/new/page.tsx
+++ apps/meetie-service/src/app/study/new/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/common/Header/Header";
+import ProgressBar from "@/components/common/ProgressBar/ProgressBar";
 
 export default function CreateStudyPage() {
   return (
@@ -8,6 +9,10 @@ export default function CreateStudyPage() {
         <Header.Title hasButton>스터디 만들기</Header.Title>
         <Header.RightText nowStep={1} />
       </Header>
+
+      <div className="pt-12">
+        <ProgressBar width="w-[50%]" />
+      </div>
     </>
   );
 }
