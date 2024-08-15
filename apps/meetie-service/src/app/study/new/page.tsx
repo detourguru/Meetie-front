4be7abb31e77f291225bd144400d072663bb16e3
sikkzz <sling0623@gmail.com>
2024-08-15 16:15:54 +0900

    feat: create input component (#21)

--- apps/meetie-service/src/app/study/new/page.tsx
+++ apps/meetie-service/src/app/study/new/page.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
+
 import Header from "@/components/common/Header/Header";
 import ProgressBar from "@/components/common/ProgressBar/ProgressBar";
+import Input from "@/components/common/Input/Input";
 
 export default function CreateStudyPage() {
   return (
@@ -12,6 +15,27 @@ export default function CreateStudyPage() {
 
       <div className="pt-12">
         <ProgressBar width="w-[50%]" />
+      </div>
+
+      <div className="px-4 pt-9">
+        <div className="flex flex-col gap-9">
+          <div className="flex flex-col gap-[10px]">
+            <h2 className="text-bold-16">모집 직군</h2>
+            <div className="rounded-lg px-4 py-3 border border-[#c4c4c4] text-blue-300 text-regular-14 flex justify-between">
+              모집 직군을 선택해주세요.
+              <Image
+                src="/svg/ic-study-down-arrow.svg"
+                alt="downArrowIcon"
+                width={16}
+                height={16}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <h2 className="text-bold-16">주제</h2>
+            <Input placeholder="스터디의 주제를 작성해주세요." maxLength={20} />
+          </div>
+        </div>
       </div>
     </>
   );
