a195055932c881df5aff045395c018fdb67e1704
sikkzz <sling0623@gmail.com>
2024-08-15 16:43:24 +0900

    refactor: classname change to const (#21)

--- apps/meetie-service/src/app/study/new/page.tsx
+++ apps/meetie-service/src/app/study/new/page.tsx
@@ -4,9 +4,12 @@ import Header from "@/components/common/Header/Header";
 import ProgressBar from "@/components/common/ProgressBar/ProgressBar";
 import Input from "@/components/common/Input/Input";
 import Textarea from "@/components/common/Textarea/Textarea";
+import Button from "@/components/common/Button/Button";
 
 export default function CreateStudyPage() {
   const inputTitleClassName = "text-bold-16 mb-[10px]";
+  const inputLengthTextClassName = "float-end text-regular-14 text-blue-300 mt-1";
+
   return (
     <>
       <Header>
@@ -15,11 +18,11 @@ export default function CreateStudyPage() {
         <Header.RightText nowStep={1} />
       </Header>
 
-      <div className="pt-12">
+      <div className="w-[375px] fixed top-12">
         <ProgressBar width="w-[50%]" />
       </div>
 
-      <div className="px-4 pt-9">
+      <div className="px-4 pt-[86px]">
         <div className="flex flex-col gap-9">
           <div>
             <h2 className={inputTitleClassName}>모집 직군</h2>
@@ -36,19 +39,28 @@ export default function CreateStudyPage() {
           <div>
             <h2 className={inputTitleClassName}>주제</h2>
             <Input placeholder="스터디의 주제를 작성해주세요." maxLength={20} />
-            <span className="float-end text-regular-14 text-blue-300 mt-1">0/20</span>
+            <span className={inputLengthTextClassName}>0/20</span>
           </div>
           <div>
             <h2 className={inputTitleClassName}>목표</h2>
             <Input placeholder="스터디의 목표를 간단히 작성해주세요." maxLength={20} />
-            <span className="float-end text-regular-14 text-blue-300 mt-1">0/20</span>
+            <span className={inputLengthTextClassName}>0/20</span>
           </div>
           <div>
             <h2 className={inputTitleClassName}>소개</h2>
             <Textarea placeholder="스터디를 설명해보세요." />
-            <span className="float-end text-regular-14 text-blue-300 mt-1">0/100</span>
+            <span className={inputLengthTextClassName}>0/100</span>
           </div>
         </div>
+      </div>
+
+      <div className="flex gap-3 px-4 mt-14 fixed bottom-3">
+        <Button variant="outline" size="sm">
+          <p className="text-bold-16 text-[#adb5bd]">이전</p>
+        </Button>
+        <Button>
+          <p className="text-bold-16 text-white">다음</p>
+        </Button>
       </div>
     </>
   );
