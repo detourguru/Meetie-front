df7c613b197634eb56ea7d4c60a55f74d4b262b5
ejinn1 <ejinn1@naver.com>
2024-08-16 16:44:46 +0900

    refactor: 주석 추가, h1 tag 2 개로 수정 (#22)

--- apps/meetie-service/src/app/onBoarding/complete/page.tsx
+++ apps/meetie-service/src/app/onBoarding/complete/page.tsx
@@ -1,23 +1,23 @@
+import Button from "@/components/common/Button/Button";
+
 export default function OnBoardingCompletePage() {
   return (
     <main className="flex flex-col h-screen">
       <article className="flex flex-col items-center w-full h-full px-5">
         <div className="w-full pt-[82px]">
-          <h1 className="mb-4 text-semibold-24 text-start">
-            <span className="block">김서희님의</span>
-            <span className="block">공개 프로필이 생성되었어요 👏</span>
-          </h1>
+          <span className="mb-4">
+            <h1 className="text-semibold-24">김서희님의</h1>
+            <h1 className="text-semibold-24">공개 프로필이 생성되었어요 👏</h1>
+          </span>
           <p className="text-gray-200 text-regular-14">나와 딱 맞는 스터디를 찾으러 떠나볼까요?</p>
         </div>
 
+        {/* TODO: #18 badge icon으로 변경 예정 */}
         <div className="w-[146px] h-[203px] mt-[77px] bg-primary-300">프로필 사진</div>
 
-        <button
-          type="button"
-          className="w-full bg-primary-500 text-semibold-16 h-[49px] text-white rounded-lg mb-[42px] mt-auto"
-        >
-          확인하러 가기
-        </button>
+        <Button size="xl" className="mb-[42px] mt-auto">
+          <span className="text-white text-semibold-16">확인하러 가기</span>
+        </Button>
       </article>
     </main>
   );
