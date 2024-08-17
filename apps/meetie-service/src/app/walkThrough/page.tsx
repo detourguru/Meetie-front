a1f2898326dfac9a43f8beae5c1275b52f07bc21
ejinn1 <ejinn1@naver.com>
2024-08-17 12:32:05 +0900

    refactor: walkthrough explain constant로 나눔 (#22)

--- apps/meetie-service/src/app/walkThrough/page.tsx
+++ apps/meetie-service/src/app/walkThrough/page.tsx
@@ -4,24 +4,10 @@ import Button from "@/components/common/Button/Button";
 import CalendarImage from "@/components/WalkThrough/CalendarImage/CalendarImage";
 import MegaPhoneImage from "@/components/WalkThrough/MegaPhoneImage/MegaPhoneImage";
 import PhoneImage from "@/components/WalkThrough/PhoneImage/PhoneImage";
+import { EXPLAIN } from "@/constants/walkThrough";
 import { useState } from "react";
 
 export default function WalkThroughPage() {
-  const EXPLAIN = [
-    {
-      TITLE: { FIRST: "다양한 IT직군과의", SECOND: "견고한 스터디를 경험해보세요." },
-      SUBTEXT: ["다른 학습자들과 소통하며 함께 성장하세요!"],
-    },
-    {
-      TITLE: { FIRST: "다른 학습자들과", SECOND: "함께 고민을 나눠보세요." },
-      SUBTEXT: ["학습에 어려움을 겪고 있나요?", "고민을 나누고 해결책을 찾아보세요!"],
-    },
-    {
-      TITLE: { FIRST: "뱃지를 통해 성취를 경험하고,", SECOND: "차별화된 신뢰지표를 만들어보세요." },
-      SUBTEXT: ["스터디 활동을 수행하고 다양한 뱃지를 획득하세요!"],
-    },
-  ];
-
   const [walkThroughNumber, setwalkThroughNumber] = useState(0);
 
   const handleClickNumber = (newNum: number) => {
