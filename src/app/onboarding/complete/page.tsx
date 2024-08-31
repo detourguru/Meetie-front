359bac51790bf54e5a1dad5ce61d936c9e0ea507
ejinn1 <ejinn1@naver.com>
2024-08-31 22:08:24 +0900

    refactor: onboarding complete page refactor (#81)

--- src/app/onboarding/complete/page.tsx
+++ src/app/onboarding/complete/page.tsx
@@ -1,17 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
 import { Suspense } from "react";
 
-import CompleteBox from "@/components/OnBoarding/CompleteBox/CompleteBox";
+import Button from "@/components/common/Button/Button";
+import OnboardingProfile from "@/components/OnBoarding/Profile/Profile";
+import OnBoardingTitle from "@/components/OnBoardingTitle/OnBoardingTitle";
+
+import { COMPLETE_DATA } from "@/constants/onBoarding";
+import { PATH } from "@/constants/path";
+
+import { useUserInformationQuery } from "@/hooks/api/userInfo/useUserInformationQuery";
 
 export default function OnBoardingCompletePage() {
+  const {
+    userData: {
+      data: { name, profileImage },
+    },
+  } = useUserInformationQuery();
+
   return (
-    <main className="flex flex-col h-screen">
+    <main className="flex flex-col items-center w-full h-full px-4">
+      <OnBoardingTitle textData={COMPLETE_DATA(name)} index={0} subTextColor="text-gray-200" />
+
       <Suspense
         fallback={
-          // TODO: profile 로딩 컴포넌트로 변경 or 공통 로딩 컴포넌트로 변경
-          <div>loading...</div>
+          <div className="relative w-[150px] mt-[77px]">
+            <Image
+              src="/svg/ic-onboarding-complete-back.svg"
+              width={153}
+              height={209}
+              alt="back"
+              priority
+              className="absolute top-0"
+            />
+          </div>
         }
       >
-        <CompleteBox />
+        <OnboardingProfile userName={name} profileImage={profileImage} />
+
+        <Link href={PATH.STUDY_ROOM_LIST} className="mt-auto mb-[42px]">
+          <Button size="xl">
+            <span className="text-white text-semibold-16">스터디 찾으러 가기</span>
+          </Button>
+        </Link>
       </Suspense>
     </main>
   );
