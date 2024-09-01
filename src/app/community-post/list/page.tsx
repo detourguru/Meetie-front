7db537ad1ee7c3d306d24035a68355affd60b262
soyoung125 <sypark041754@gmail.com>
2024-09-02 00:37:16 +0900

    style: change search input placeholder color (#79)

--- src/app/community-post/list/page.tsx
+++ src/app/community-post/list/page.tsx
@@ -16,7 +16,7 @@ export default function CommunityPostListPage() {
       <div className="flex gap-3 mx-4 mt-14 bg-[#F1F3F5] rounded-lg px-3.5 py-3">
         <Image src="/svg/ic-community-search.svg" alt="search" width={24} height={24} />
         <input
-          className="outline-none bg-[#F1F3F5] text-regular-14 text-[#ADB5BD]"
+          className="outline-none bg-[#F1F3F5] text-regular-14 placeholder:text-[#ADB5BD]"
           placeholder="관심있는 주제를 검색해보세요"
         />
       </div>
