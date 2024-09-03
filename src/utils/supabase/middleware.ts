b07f89e2d0c42fc9d1a05916d5875727c5b9aa5c
ejinn1 <ejinn1@naver.com>
2024-09-03 17:01:35 +0900

    refactor: middleware add root to auth redirect (#81)

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -32,7 +32,7 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (user && request.nextUrl.pathname === "/login") {
+  if ((user && request.nextUrl.pathname === "/login") || request.nextUrl.pathname === "/") {
     const url = request.nextUrl.clone();
 
     url.pathname = "/auth/redirect";
