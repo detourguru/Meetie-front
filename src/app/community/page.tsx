03979bd8eeb2680bca0ffcf714023da0566ae7fd
soyoung125 <sypark041754@gmail.com>
2024-09-03 15:42:18 +0900

    refactor: add TODO comment, fix input length (#79)

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -14,11 +14,13 @@ export default function CommunityPostListPage() {
         <Header.RightButton icon="/svg/ic-header-question.svg" />
       </Header>
 
+      {/* TODO: 공통 컴포넌트 제작 */}
       <div className="flex gap-3 mx-4 mt-14 bg-[#F1F3F5] rounded-lg px-3.5 py-3">
         <Image src="/svg/ic-community-search.svg" alt="search" width={24} height={24} />
         <input
-          className="outline-none bg-[#F1F3F5] text-regular-14 placeholder:text-[#ADB5BD]"
+          className="outline-none bg-[#F1F3F5] text-regular-14 placeholder:text-[#ADB5BD] w-full"
           placeholder="관심있는 주제를 검색해보세요"
+          maxLength={20}
         />
       </div>
 
