7d404e552487eefcebc63edb6b87da9587fe73d8
ejinn1 <ejinn1@naver.com>
2024-09-03 15:12:56 +0900

    refactor: signup query key delete (#81)

--- src/hooks/api/signup/usePostSignUpMutation.ts
+++ src/hooks/api/signup/usePostSignUpMutation.ts
@@ -2,18 +2,9 @@ import { useMutation } from "@tanstack/react-query";
 
 import { postSignUp } from "@/apis/signup/postSignup";
 
-import { queryClient } from "@/components/providers/QueryProvider";
-
-import { QUERY_KEYS } from "@/constants/queryKey";
-
 export const usePostSignUpMutation = () => {
   const postSignUpMutation = useMutation({
     mutationFn: postSignUp,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEYS.SIGNUP],
-      });
-    },
   });
 
   return postSignUpMutation;
