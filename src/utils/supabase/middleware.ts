ae18e7e9de2944563fead178b7df851f89ecaeec
ejinn1 <ejinn1@naver.com>
2024-09-04 22:36:30 +0900

    refactor: middleware add onboarding check (#81)

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -32,12 +32,33 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (user && (request.nextUrl.pathname === "/login" || request.nextUrl.pathname === "/")) {
-    const url = request.nextUrl.clone();
+  if (user) {
+    if (request.nextUrl.pathname === "/login" || request.nextUrl.pathname === "/") {
+      const url = request.nextUrl.clone();
 
-    url.pathname = "/auth/redirect";
+      url.pathname = "/walk-through";
 
-    return NextResponse.redirect(url);
+      return NextResponse.redirect(url);
+    }
+
+    if (
+      request.nextUrl.pathname === "/walk-through" ||
+      request.nextUrl.pathname === "/onboarding"
+    ) {
+      const { data } = await supabase
+        .from("userinfo")
+        .select("onboardingCheck")
+        .eq("user_id", user.id)
+        .single();
+
+      if (data && data.onboardingCheck) {
+        const url = request.nextUrl.clone();
+
+        url.pathname = "/study-room/list";
+
+        return NextResponse.redirect(url);
+      }
+    }
   }
 
   if (
