6cd80862dd66628b7a596627c2adc5c1aa8b19ad
김채연 <detourguru@lucia.local>
2024-09-10 11:16:01 +0900

    refactor: search bar type change to common & search added to study explorer (#97)

---
+++ src/constants/member.ts
@@ -0,0 +1,8 @@
+import type { SelectOptionType } from "@/types/filter";
+
+export const JOB_OPTION_DATA: SelectOptionType[] = [
+  { value: "직무", label: "all" },
+  { value: "개발", label: "develop" },
+  { value: "디자인", label: "design" },
+  { value: "기획", label: "plan" },
+];
