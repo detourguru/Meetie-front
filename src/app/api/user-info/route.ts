d38ce4e861b61cbe597506b0f384d5b1a808fa54
soyoung125 <sypark041754@gmail.com>
2024-09-10 13:34:07 +0900

    fix: fix delete user error (#101)

--- src/app/api/user-info/route.ts
+++ src/app/api/user-info/route.ts
@@ -28,28 +28,33 @@ export async function DELETE(request: Request) {
     const { searchParams } = new URL(request.url);
     const userId = searchParams.get("user_id");
 
-    if (userId) {
-      Promise.all([
-        supabase.from("task_confirm").update({ user_id: newUserId }).eq("user_id", userId),
-        supabase.from("task").update({ user_id: newUserId }).eq("user_id", userId),
-        supabase.from("study_room").update({ owner_id: newUserId }).eq("owner_id", userId),
-        supabase.from("study").update({ user_id: newUserId }).eq("user_id", userId),
-        supabase.from("community").update({ user_id: newUserId }).eq("user_id", userId),
-      ])
-        .then(async () => {
-          const { error } = await supabaseAdmin.auth.admin.deleteUser(userId);
-
-          if (!error) {
-            return NextResponse.json({ message: "ok" }, { status: 200 });
-          }
-          return NextResponse.json({ message: "error" }, { status: 400 });
-        })
-        .catch((error) => {
-          return NextResponse.json({ message: error.error }, { status: 400 });
-        });
+    if (!userId) {
+      return NextResponse.json({ message: "error" }, { status: 400 });
     }
 
-    return NextResponse.json({ message: "error" }, { status: 400 });
+    const res = await Promise.all([
+      supabase.from("task_confirm").update({ user_id: newUserId }).eq("user_id", userId),
+      supabase.from("task").update({ user_id: newUserId }).eq("user_id", userId),
+      supabase.from("study_room").update({ owner_id: newUserId }).eq("owner_id", userId),
+      supabase.from("study").update({ user_id: newUserId }).eq("user_id", userId),
+      supabase.from("community").update({ user_id: newUserId }).eq("user_id", userId),
+    ])
+      .then(async () => {
+        const { error } = await supabaseAdmin.auth.admin.deleteUser(userId);
+
+        if (!error) {
+          return NextResponse.json({ message: "ok" }, { status: 200, statusText: "ok" });
+        }
+        return NextResponse.json({ message: "error" }, { status: 400, statusText: "error" });
+      })
+      .catch((error) => {
+        return NextResponse.json(
+          { message: error.error },
+          { status: 400, statusText: error.error },
+        );
+      });
+
+    return NextResponse.json({ message: res.statusText }, { status: res.status });
   } catch (error) {
     return NextResponse.json({ message: "error" }, { status: 500 });
   }
