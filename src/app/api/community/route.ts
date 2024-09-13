293ad7ca0c7660c5622fd69beeddc607b37208a1
soyoung125 <sypark041754@gmail.com>
2024-09-13 15:55:30 +0900

    refactor: remove community userPosition (#120)

--- src/app/api/community/route.ts
+++ src/app/api/community/route.ts
@@ -8,15 +8,7 @@ export async function POST(request: Request) {
 
     const data = await request.json();
 
-    const { data: user } = await supabase.from("userinfo").select("user_id").single();
-
-    const { data: onboarding } = await supabase.from("onboarding").select("position").single();
-
-    const { error } = await supabase.from("community").insert({
-      ...data,
-      user_id: user?.user_id,
-      userPosition: onboarding?.position ?? "",
-    });
+    const { error } = await supabase.from("community").insert(data);
 
     if (!error) {
       return NextResponse.json({ message: "ok" }, { status: 200 });
