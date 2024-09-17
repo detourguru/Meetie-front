84d958c46b590d80c9b4b2319b11c4faeab8951a
ejinn1 <ejinn1@naver.com>
2024-09-17 18:51:02 +0900

    refactor: progress bar add transition width (#128)

--- src/components/common/ProgressBar/ProgressBar.tsx
+++ src/components/common/ProgressBar/ProgressBar.tsx
@@ -5,7 +5,9 @@ interface ProgressBarProps {
 const ProgressBar = ({ width }: ProgressBarProps) => {
   return (
     <div className="w-full h-0.5 bg-[#DEE2E6]">
-      <div className={`${width} h-0.5 bg-primary-500`} />
+      <div
+        className={`${width} h-0.5 bg-primary-500 trnasition-[width] duration-300 ease-in-out`}
+      />
     </div>
   );
 };
