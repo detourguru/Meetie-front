e9334be58064e0d3d0d9a8e8e4215c90e5d5653c
soyoung125 <sypark041754@gmail.com>
2024-09-18 19:31:46 +0900

    feat: create bookmark list page (#133)

--- src/app/api/user-info/route.ts
+++ src/app/api/user-info/route.ts
@@ -34,11 +34,18 @@ export async function GET(request: Request) {
     const { data } = await query;
 
     const currentDate = new Date().toISOString();
+    const bookmarks = data.bookmarks
+      .filter((bookmark: { study_id: string; isMarked: boolean }) => bookmark.isMarked)
+      .map((bookmark: { study_id: string; isMarked: boolean }) => bookmark.study_id);
 
-    const [studyList, lastStudyList] = await Promise.all([
+    const [studyListData, lastStudyListData, bookmarkStudyList] = await Promise.all([
       supabase.from("study_room").select("id").in("id", data.studyList).gt("endDate", currentDate),
       supabase.from("study_room").select("id").in("id", data.studyList).lte("endDate", currentDate),
+      supabase.from("study").select(`*, bookmarks(isMarked)`).in("id", bookmarks),
     ]);
+
+    const studyList = studyListData.data?.map((study: { id: string }) => study.id) ?? [];
+    const lastStudyList = lastStudyListData.data?.map((study: { id: string }) => study.id) ?? [];
 
     return NextResponse.json({
       message: "ok",
