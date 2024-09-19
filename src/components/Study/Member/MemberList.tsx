98d70042d7ee350042e6ee66655e13cb6ff8ec20
김채연 <detourguru@lucia.local>
2024-09-19 10:32:41 +0900

    refactor: exclude secessioned user from memberList (#108)

--- src/components/Study/Member/MemberList.tsx
+++ src/components/Study/Member/MemberList.tsx
@@ -4,11 +4,13 @@ import { useAllUserInfoQuery } from "@/hooks/api/userInfo/useAllUserInfoQuery";
 
 const MemberList = () => {
   const { data } = useAllUserInfoQuery();
+  // FIXME: 탈퇴 유저 flag 값 받아서 분기 태우도록 수정 필요
+  const user = data.data.filter((member) => !member.name.includes("탈퇴"));
   return (
     <>
       <div className="p-2 w-full grid grid-rows-2 grid-cols-2 gap-2">
-        {data.data &&
-          data.data.map((member) => (
+        {user &&
+          user.map((member) => (
             <Member
               name={member.name}
               description={member.styles.join("|")}
