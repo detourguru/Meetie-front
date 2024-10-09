3251eb182368f0adbb782b77948a7eb3718cded8
ejinn1 <ejinn1@naver.com>
2024-10-09 12:31:07 +0900

    refactor: error message change (#150)

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -1,21 +1,21 @@
 export const ERROR_MESSAGE = {
   SIGNUP: (message: string) => {
     const errorMessages: { [key: string]: string } = {
-      "exist user error": "이미 가입된 계정입니다. 로그인해주세요.",
-      "signup error": "회원가입에 문제가 발생했습니다. 다시 시도해주세요.",
-      "server error": "서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.",
+      "exist user error": "이미 가입된 계정입니다",
+      "signup error": "회원가입에 문제가 발생했습니다",
+      "server error": "서버에 문제가 발생했습니다",
     };
 
-    return errorMessages[message] || "알 수 없는 문제가 발생했습니다. 다시 시도해주세요.";
+    return errorMessages[message] || "알 수 없는 문제가 발생했습니다";
   },
   LOGIN: (message: string) => {
     const loginErrorMessages: { [key: string]: string } = {
-      "not exist user error": "존재하지 않는 사용자입니다.",
-      "wrong password error": "비밀번호가 잘못되었습니다.",
-      "fail to login error": "로그인에 실패했습니다.",
-      "server error": "서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.",
+      "not exist user error": "존재하지 않는 사용자입니다",
+      "wrong password error": "비밀번호가 잘못되었습니다",
+      "fail to login error": "로그인에 실패했습니다",
+      "server error": "서버에 문제가 발생했습니다",
     };
 
-    return loginErrorMessages[message] || "알 수 없는 문제가 발생했습니다. 다시 시도해주세요.";
+    return loginErrorMessages[message] || "알 수 없는 문제가 발생했습니다";
   },
 };
