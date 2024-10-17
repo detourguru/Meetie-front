b82ae29d77196694e09b5922c8e3dd3448d5e01b
sikkzz <sling0623@gmail.com>
2024-10-17 14:33:12 +0900

    test: study explorer member search test code add (#159)

--- src/e2e/study-explorer.spec.ts
+++ src/e2e/study-explorer.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("스터디 탐색 페이지", async ({ page }) => {
-  await test.step("스터디 검색", async () => {
+  await test.step("스터디 검색 후 클릭 시 스터디 페이지로 이동", async () => {
     await page.goto("/study-explorer");
     await page.waitForURL("/study-explorer");
 
@@ -19,7 +19,7 @@ test("스터디 탐색 페이지", async ({ page }) => {
     await expect(page.getByRole("heading", { name: "넥스트 공부" })).toBeVisible();
   });
 
-  await test.step("태그 필터링", async () => {
+  await test.step("태그 필터링 후 클릭 시 스터디 페이지로 이동", async () => {
     await page.goto("/study-explorer");
     await page.waitForURL("/study-explorer");
 
@@ -32,8 +32,24 @@ test("스터디 탐색 페이지", async ({ page }) => {
     await expect(page.getByText("프론트엔드", { exact: true })).toBeVisible();
   });
 
-  //   await test.step("스터디 페이지 이동", async () => {
-  //     await page.getByText("넥스트").click();
-  //     await expect(page.getByText("스터디 목표")).toBeVisible();
-  //   });
+  await test.step("팀원 검색 후 클릭 시 오픈 프로필 페이지로 이동", async () => {
+    await page.goto("/study-explorer");
+    await page.waitForURL("/study-explorer");
+
+    await page.getByText("팀원 찾기").click();
+
+    await page.getByTestId("searchSheetOpen").click();
+
+    await expect(page.getByTestId("searchButton")).toBeVisible();
+
+    await page.getByTestId("searchInput").fill("박소영");
+
+    await page.getByTestId("searchButton").click();
+
+    await page.getByText("박소영").nth(2).click();
+
+    await expect(page.getByRole("heading", { name: "오픈 프로필" })).toBeVisible();
+
+    await expect(page.getByText("박소영")).toBeVisible();
+  });
 });
