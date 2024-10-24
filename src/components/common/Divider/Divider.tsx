71f708f3270d7dfdff02990594bde0d05e2b5e73
sikkzz <sling0623@gmail.com>
2024-10-24 19:22:05 +0900

    refactor: divider component refactoring (#170)

--- src/components/common/Divider/Divider.tsx
+++ src/components/common/Divider/Divider.tsx
@@ -1,27 +1,24 @@
 import React from "react";
 
-import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "@/utils/className";
 
-const dividerVariants = cva("", {
-  variants: {
-    direction: {
-      horizontal: "w-full h-[1px]",
-      vertical: "w-[1px] h-full",
-    },
+const dividerStyles = {
+  direction: {
+    horizontal: "w-full h-[1px]",
+    vertical: "w-[1px] h-full",
   },
-  defaultVariants: {
-    direction: "horizontal",
-  },
-});
-
-export interface DividerProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof dividerVariants> {}
-
-const Divider = ({ className, direction, ...props }: DividerProps) => {
-  return <div className={cn(dividerVariants({ direction, className }))} {...props} />;
 };
+
+export interface DividerProps extends React.HTMLAttributes<HTMLDivElement> {
+  direction?: keyof typeof dividerStyles.direction;
+}
+
+const Divider = React.forwardRef<HTMLDivElement, DividerProps>(
+  ({ className, direction = "horizontal", ...props }, ref) => {
+    const classes = cn(dividerStyles.direction[direction], className);
+
+    return <div className={classes} {...props} ref={ref} />;
+  },
+);
 
 export default Divider;
